44fad3ede1195c93aac817cb096f723fca7ba313
javie <javie@JavierPC>
2021-11-11 20:35:56 +0100

    prueba clase productoSiplementos

---
+++ MySupplements/src/Clases/ProductoSuplementos.java
@@ -0,0 +1,43 @@
+package Clases;
+
+import java.util.ArrayList;
+
+public class ProductoSuplementos extends Producto {
+	
+	private int proteinas;
+	private int calorias;
+	private int grasas;
+	private int hidratos;
+	
+	
+	@Override
+	public String toString() {
+		return "ProductoSuplementos [proteinas=" + proteinas + ", calorias=" + calorias + ", grasas=" + grasas
+				+ ", hidratos=" + hidratos + "]";
+	}
+	public int getProteinas() {
+		return proteinas;
+	}
+	public void setProteinas(int proteinas) {
+		this.proteinas = proteinas;
+	}
+	public int getCalorias() {
+		return calorias;
+	}
+	public void setCalorias(int calorias) {
+		this.calorias = calorias;
+	}
+	public int getGrasas() {
+		return grasas;
+	}
+	public void setGrasas(int grasas) {
+		this.grasas = grasas;
+	}
+	public int getHidratos() {
+		return hidratos;
+	}
+	public void setHidratos(int hidratos) {
+		this.hidratos = hidratos;
+	}
+	
+}
